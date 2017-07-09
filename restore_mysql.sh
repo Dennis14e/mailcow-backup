4cbc255b17685ad5f506da65e607fee9c61ea9a2
Dennis Neufeld <info@dennis14e.de>
2017-07-09 12:58:35 +0200

    Restore scripts used configuration file

--- restore_mysql.sh
+++ restore_mysql.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-## SETTINGS
-CUR_DIR=$(dirname "$(readlink -f "$0")")
-CUR_DIR=${CUR_DIR%/}
-
-MAILCOW_DIR="/home/mailcow-dockerized"
-MAILCOW_DIR=${MAILCOW_DIR%/}
-
-BACKUP_DIR="${CUR_DIR}/mysql"
-BACKUP_DIR=${BACKUP_DIR%/}
-
-
-## DO NOT CHANGE
-
-# WARNING
-echo "WARNING! This script has not yet been tested!"
-echo "The creator of this script is not responsible for data loss!"
+# Warning
+echo "###################################################"
+echo "#  WARNING! This script has not yet been tested!  #"
+echo "# No liability is assumed for damage of any kind! #"
+echo "###################################################"
+echo
 
 while true
 do
@@ -33,49 +23,87 @@ do
 done
 
 
-# check mailcow.conf
-MAILCOW_CFG="${MAILCOW_DIR}/mailcow.conf"
+# directory of this script
+CUR_DIR=$(dirname "$(readlink -f "$0")")
+CUR_DIR=${CUR_DIR%/}
 
-echo "Check, if \"${MAILCOW_CFG}\" exists..."
-if [ ! -f "${MAILCOW_CFG}" ]
+
+# load configuration
+if [ ! -f "${CUR_DIR}/backup.conf" ]
 then
-  >&2 echo "mailcow.conf not found."
+  >&2 echo "Configuration file \"${CUR_DIR}/backup.conf\" was not found."
   exit 1
+fi
+
+echo "Import backup variables."
+source "${CUR_DIR}/backup.conf"
+
+
+# required backup variables available?
+if [ -z ${MAILCOW_DIR+x} ] || [ -z ${BACKUP_MYSQL_DIR+x} ]
+then
+  >&2 echo "Required variables do not exist (backup)."
+  exit 1
+fi
+
+
+# Rename variables
+BACKUP_DIR="${BACKUP_MYSQL_DIR}"
+
+# remove trailing slash
+MAILCOW_DIR=${MAILCOW_DIR%/}
+BACKUP_DIR=${BACKUP_DIR%/}
+
+# absolute/relative path?
+if [[ "${BACKUP_DIR}" != /* ]]
+then
+  BACKUP_DIR="${CUR_DIR}/${BACKUP_DIR}"
+fi
+
+
+# check mailcow installation
+if [ ! -f "${MAILCOW_DIR}/mailcow.conf" ]
+then
+  >&2 echo "File \"${MAILCOW_DIR}/mailcow.conf\" was not found."
+  exit 1
+else
+  echo "File \"${MAILCOW_DIR}/mailcow.conf\" was found."
 fi
 
 
 # source variables
-echo "Import mailcow variables..."
-source "${MAILCOW_CFG}"
+echo "Import mailcow variables."
+source "${MAILCOW_DIR}/mailcow.conf"
 
 
-# check variables
-echo "Check mailcow variables..."
+# required mailcow variables available?
 if [ -z ${DBUSER+x} ] || [ -z ${DBPASS+x} ] || [ -z ${DBNAME+x} ]
 then
-  >&2 echo "variables unset"
+  >&2 echo "Required variables do not exist (mailcow)."
   exit 1
 fi
 
 
-# choose file
-PS3="Type a number: "
+# choose backup file
 BACKUP_FILEPATH=""
 FILE_LIST=$(find "${BACKUP_DIR}/" -maxdepth 1 -type f -name "*.sql")
+PS3="Type a number: "
 
+echo
+echo "Select a backup file:"
 select FILE_SELECT in $FILE_LIST "Quit"
 do
   case $FILE_SELECT in
     *.sql)
       BACKUP_FILEPATH="${FILE_SELECT}"
-      echo "You selected file \"${FILE_SELECT}\""
+      echo "You selected file \"${FILE_SELECT}\"."
       break
       ;;
     "Quit")
       exit 0
       ;;
     *)
-      echo "This is not a valid number"
+      echo "This is not a valid number."
       ;;
   esac
 done
@@ -85,19 +113,21 @@ done
 BACKUP_FILENAME=$(basename "${BACKUP_FILEPATH}")
 BACKUP_FILEPATH="${BACKUP_DIR}/${BACKUP_FILENAME}"
 
+
+# check BACKUP_FILEPATH
 if [ ! -f "${BACKUP_FILEPATH}" ]
 then
-  >&2 echo "File does not exist"
+  >&2 echo "Backup file does not exist."
   exit 1
 fi
 
 
-# docker instance id
+# get docker instance id of mysql-mailcow
 DOCKER_INSTANCE_ID=$(cd "${MAILCOW_DIR}" && docker-compose ps -q mysql-mailcow)
 
 if [ $? -ne 0 ] || [ -z "${DOCKER_INSTANCE_ID}" ]
 then
-  >&2 echo "Can not get docker instance id"
+  >&2 echo "Could not get docker instance id."
   exit 1
 fi
 
@@ -105,9 +135,11 @@ fi
 # docker restore
 docker exec -i "${DOCKER_INSTANCE_ID}" mysql "-u${DBUSER}" "-p${DBPASS}" "${DBNAME}" < "${BACKUP_FILEPATH}"
 
-if [ $? -ne 0 ]
+if [ $? -eq 0 ]
 then
-  >&2 echo "An error occurred"
+  echo "Backup was successfully restored."
+else
+  >&2 echo "There was an error restoring the backup."
   exit 1
 fi
 
--- restore_vmail.sh
+++ restore_vmail.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-## SETTINGS
-CUR_DIR=$(dirname "$(readlink -f "$0")")
-CUR_DIR=${CUR_DIR%/}
-
-MAILCOW_DIR="/home/mailcow-dockerized"
-MAILCOW_DIR=${MAILCOW_DIR%/}
-
-BACKUP_DIR="${CUR_DIR}/vmail"
-BACKUP_DIR=${BACKUP_DIR%/}
-
-
-## DO NOT CHANGE
-
-# WARNING
-echo "WARNING! This script has not yet been tested!"
-echo "The creator of this script is not responsible for data loss!"
+# Warning
+echo "###################################################"
+echo "#  WARNING! This script has not yet been tested!  #"
+echo "# No liability is assumed for damage of any kind! #"
+echo "###################################################"
+echo
 
 while true
 do
@@ -33,24 +23,74 @@ do
 done
 
 
-# choose file
-PS3="Type a number: "
+# directory of this script
+CUR_DIR=$(dirname "$(readlink -f "$0")")
+CUR_DIR=${CUR_DIR%/}
+
+
+# load configuration
+if [ ! -f "${CUR_DIR}/backup.conf" ]
+then
+  >&2 echo "Configuration file \"${CUR_DIR}/backup.conf\" was not found."
+  exit 1
+fi
+
+echo "Import backup variables."
+source "${CUR_DIR}/backup.conf"
+
+
+# required backup variables available?
+if [ -z ${MAILCOW_DIR+x} ] || [ -z ${BACKUP_VMAIL_DIR+x} ]
+then
+  >&2 echo "Required variables do not exist (backup)."
+  exit 1
+fi
+
+
+# Rename variables
+BACKUP_DIR="${BACKUP_VMAIL_DIR}"
+
+# remove trailing slash
+MAILCOW_DIR=${MAILCOW_DIR%/}
+BACKUP_DIR=${BACKUP_DIR%/}
+
+# absolute/relative path?
+if [[ "${BACKUP_DIR}" != /* ]]
+then
+  BACKUP_DIR="${CUR_DIR}/${BACKUP_DIR}"
+fi
+
+
+# check mailcow installation
+if [ ! -f "${MAILCOW_DIR}/mailcow.conf" ]
+then
+  >&2 echo "File \"${MAILCOW_DIR}/mailcow.conf\" was not found."
+  exit 1
+else
+  echo "File \"${MAILCOW_DIR}/mailcow.conf\" was found."
+fi
+
+
+# choose backup file
 BACKUP_FILEPATH=""
 FILE_LIST=$(find "${BACKUP_DIR}/" -maxdepth 1 -type f -name "*.tar.gz")
+PS3="Type a number: "
 
+echo
+echo "Select a backup file:"
 select FILE_SELECT in $FILE_LIST "Quit"
 do
   case $FILE_SELECT in
     *.tar.gz)
       BACKUP_FILEPATH="${FILE_SELECT}"
-      echo "You selected file \"${FILE_SELECT}\""
+      echo "You selected file \"${FILE_SELECT}\"."
       break
       ;;
     "Quit")
       exit 0
       ;;
     *)
-      echo "This is not a valid number"
+      echo "This is not a valid number."
       ;;
   esac
 done
@@ -60,29 +100,31 @@ done
 BACKUP_FILENAME=$(basename "${BACKUP_FILEPATH}")
 BACKUP_FILEPATH="${BACKUP_DIR}/${BACKUP_FILENAME}"
 
+
+# check BACKUP_FILEPATH
 if [ ! -f "${BACKUP_FILEPATH}" ]
 then
-  >&2 echo "File does not exist"
+  >&2 echo "Backup file does not exist."
   exit 1
 fi
 
 
-# docker instance id
+# get docker instance id of dovecot-mailcow
 DOCKER_INSTANCE_ID=$(cd "${MAILCOW_DIR}" && docker-compose ps -q dovecot-mailcow)
 
 if [ $? -ne 0 ] || [ -z "${DOCKER_INSTANCE_ID}" ]
 then
-  >&2 echo "Can not get docker instance id"
+  >&2 echo "Could not get docker instance id."
   exit 1
 fi
 
 
-# docker volume name
+# get docker volume name
 DOCKER_VOLUME_NAME=$(docker inspect --format '{{ range .Mounts }}{{ if eq .Destination "/var/vmail" }}{{ .Name }}{{ end }}{{ end }}' "${DOCKER_INSTANCE_ID}")
 
 if [ $? -ne 0 ] || [ -z "${DOCKER_VOLUME_NAME}" ]
 then
-  >&2 echo "Can not get docker volume name"
+  >&2 echo "Could not get docker volume name."
   exit 1
 fi
 
@@ -90,9 +132,11 @@ fi
 # docker restore
 docker run --rm -it -v "${DOCKER_VOLUME_NAME}:/vmail" -v "${BACKUP_DIR}:/backup" debian:jessie tar xvfz "/backup/${BACKUP_FILENAME}"
 
-if [ $? -ne 0 ]
+if [ $? -eq 0 ]
 then
-  >&2 echo "An error occurred"
+  echo "Backup was successfully restored."
+else
+  >&2 echo "There was an error restoring the backup."
   exit 1
 fi
 
